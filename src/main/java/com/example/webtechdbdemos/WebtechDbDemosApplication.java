11b2168357d3816021662ed6b20e6d676340d7ac
marwin.moellers <moellers@htw-berlin.de>
2023-11-01 15:37:43 +0100

    init

---
+++ src/main/java/com/example/webtechdbdemos/WebtechDbDemosApplication.java
@@ -0,0 +1,13 @@
+package com.example.webtechdbdemos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebtechDbDemosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebtechDbDemosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webtechdbdemos/WebtechDbDemosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webtechdbdemos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebtechDbDemosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
