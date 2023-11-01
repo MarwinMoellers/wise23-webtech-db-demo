530d425d376af01ab6c19c025eab23ef9870fd53
marwin.moellers <moellers@htw-berlin.de>
2023-11-01 15:56:29 +0100

    db anbindung

--- src/main/java/com/example/webtechdbdemos/service/PizzaService.java
+++ src/main/java/com/example/webtechdbdemos/service/PizzaService.java
@@ -1,14 +1,21 @@
 package com.example.webtechdbdemos.service;
 
 import com.example.webtechdbdemos.model.Pizza;
+import com.example.webtechdbdemos.respository.PizzaRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class PizzaService {
+
+    private final PizzaRepository pizzaRepository;
+
     public Pizza createPizza(Pizza pizza) {
         pizza.setId(UUID.randomUUID().toString());
+        pizzaRepository.save(pizza);
         return pizza;
     }
 }
