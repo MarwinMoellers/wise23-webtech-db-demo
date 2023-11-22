49bc899dd984affe53e3c12fe454d3cb9a485c9d
marwin.moellers <moellers@htw-berlin.de>
2023-11-22 16:11:21 +0100

    add pizza backend to ui

---
+++ ui/src/views/PizzaOverview.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      pizzas: {}
+    };
+  },
+  mounted() {
+    axios
+        .get('http://localhost:8088/api/pizza')
+        .then((response) => {
+          this.pizzas = response.data;
+        });
+  }
+};
+</script>
+
+<template>
+  <div v-for="pizza in pizzas" :key="pizza.id">
+    <p>{{ pizza.id }}</p>
+    {{ pizza.name }}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
